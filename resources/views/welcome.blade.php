d14f85233eca11a344a9742aa38a31dda863db39
felixkiryowa <franciskiryowa68@gmail.com>
2020-06-02 13:37:16 +0300

    Editing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -82,6 +82,7 @@
             <div class="content">
                 <div class="title m-b-md">
                     ECommerce API
+                    This is a test api for flutter
                 </div>
             </div>
         </div>
